2c1cef8f440685714bbca75e4c54a3ab52c58014
Diana <dianamakaryan2005@gmail.com>
2024-11-26 00:50:06 +0200

    changes
    removed unnecessary lines of code
    added schedule restrictions

--- Code.py
+++ Code.py
@@ -9,39 +9,17 @@ def read_dataset(file_path):
                 points.append((x, y))
     return points
 
-def plot_points_and_save(dataset_path, output_path):
-    """
-    Відображає точки з датасету на координатній площині та зберігає графік у файл.
-
-    Функція зчитує пари координат точок із заданого текстового файлу, будує графік із точками 
-    на координатній площині, показує його на екрані та зберігає у файл у форматі PNG.
-
-    Параметри:
-    -----------
-    dataset_path : str
-        Шлях до текстового файлу, який містить координати точок .
-    output_path : str
-        Шлях до файлу для збереження зображення.
-    """
-    
+# функція для виведення точок на екран та збереження у файл png
+def plot_points_and_save(dataset_path, output_path): 
     # Зчитуємо координати
     points = read_dataset(dataset_path)
-    
-    # Якщо немає точок у файлі
-    if not points:
-        print("Файл порожній або не містить правильних даних.")
-        return
-    
-    #Витягуємо координати X і Y
     x_coords, y_coords = zip(*points)
     
     #Створюємо полотно
     canvas_width, canvas_height = 960, 540
-    dpi = 100  # Щільність точок на дюйм
+    dpi = 100 
     figsize = (canvas_width / dpi, canvas_height / dpi)
-
     plt.figure(figsize = figsize)
-
 
     #виводимо точки
     plt.scatter(x_coords, y_coords, color = 'black', s = 10)
@@ -50,6 +28,8 @@ def plot_points_and_save(dataset_path, output_path):
     plt.title("Координатна площина з точками", fontsize = 14)
     plt.xlabel("X", fontsize = 12)
     plt.ylabel("Y", fontsize = 12)
+    plt.xlim(0, canvas_width)
+    plt.ylim(0, canvas_height)
 
     #Зберігаємо результат у файл
     plt.savefig(output_path, dpi = dpi)  # Збереження з роздільною здатністю 96 DPI
